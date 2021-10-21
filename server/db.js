cbc4a8a1efced8af4c7b63d58ba12b123ddc31e3
Boymeetsworld <83832599+Boymeetsworld@users.noreply.github.com>
2021-10-21 17:23:44 -0400

    added db.js

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const { Sequelize, DataTypes, Model } = require('sequelize')
+
+
+const db = new Sequelize('database', 'username', 'password', {
+    dialect: 'sqlite',
+    storage: './server/db.sqlite',
+    logging: false
+})
+
+
+
+module.exports = { db, DataTypes, Model }
