7cf26488dd8d958107e3369fc4b85a75827d6cbd
ianmkinney <ikinney51@gmail.com>
2021-10-22 16:08:35 -0400

    Added updates to server routes to be used by components

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 const seed = require('./seed')
 const { db } = require('./db')
-const { Music, User } = require('./models/index')
+const { Motivation, User } = require('./models/index')
 
 // use seed to populate database
 seed()
@@ -14,6 +14,39 @@ seed()
 // Use js libraries for server
 app.use(express.json())
 app.use(cors())
+
+/** Route for returning one random motivation */
+app.get('/getMotivation', async (req,res) => {
+    let pk = Math.floor(Math.random() * 33) + 1
+    let motivation = await Motivation.findByPk(pk)
+    res.json(motivation)
+})
+
+/** Route for return motivation by passed pk param */
+app.get('/getMotivation/:id', async (req,res) => {
+    let pk = req.params.id
+    let motivation = await Motivation.findByPk(pk)
+    return res.json(motivation)
+})
+
+/** Route for returning all motivations */
+app.get('/allMotivations', async (req,res) => {
+    let allMotivations = await Motivation.findAll()
+    res.json(allMotivations)
+})
+
+/** Route for returning one user from passed pk param */
+app.get('/user/:id', async (req,res) => {
+    let pk = req.params.id
+    let user = await User.findByPk(pk)
+    return res.json(user)
+})
+
+/** Route to return all users in the database */
+app.get('/allUsers', async (req,res) => {
+    let users = await User.findAll()
+    return res.json(users)
+})
 
 // set up app to listen on set port
 app.listen( PORT, () => {
