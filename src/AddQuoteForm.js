e3b947df22f85524e713ce70bd183cb78e49f640
Aleyda Davalos <aledavalos@Aleydas-MacBook-Pro.local>
2021-10-26 11:38:00 -0700

    added the alert when a user adds a quote to the database

--- src/AddQuoteForm.js
+++ src/AddQuoteForm.js
@@ -7,12 +7,13 @@ const CreateQuote = () =>{
   //creating a react hook to store the quote body
   const [quote,setNewQuote] = useState('')
   const [author, setQuoteAuthor] = useState('')
-  
 
   const handleSubmit = async (e) => { //creating a function to be executed when the submit button is pressed
 
     e.preventDefault() //going to stop the page from refresehing after submitting
+
       const newQuote = {quote,author}  //created a variable to store the new objest values set by the user
+
         try {
           const response = await fetch(`http://localhost:3000/motivation/add` ,{ //adding the route to be executed by the server
           method: 'POST',  //the type of request
@@ -20,9 +21,11 @@ const CreateQuote = () =>{
           body: JSON.stringify(newQuote) //turning the new object into a string to pass through the server 
           }).then((res)=>{
             console.log(res) //logging success if the request went through with updating the database
+            alert('Your quote has been added.')
           })
         }catch(err){
           console.error(err) //logging error if the request was unsuccessful
+          alert('Unable to add your quote')
         }
   }
     
@@ -32,6 +35,12 @@ const CreateQuote = () =>{
       {/* Header for the form */}
       <h2> Add your quote below: </h2>
           {/* initializing the form */}
+
+          {/* Displaying the quote for the user to see how it will show display in the end */}
+          <h4>How your quote will be saved:</h4>
+          <p> "{quote}"</p> 
+          <p>-{author}</p>
+
         <form onSubmit={handleSubmit}>
             
             {/* Adding field to input quote context */}
@@ -62,10 +71,7 @@ const CreateQuote = () =>{
 
             {/* adding a submit button to save the changes */}
             <button> Submit </button>
-
-            {/* testing to see if the setNewQuote works */}
-            <p> {quote}</p> 
-            <p>{author}</p>
+            
 
         </form>
     </div>
