fb44fc26de1dcf8e39941d3c794ce79b861ccda6
ianmkinney <ikinney51@gmail.com>
2021-10-28 18:18:07 -0400

    Added simple styling updates and added login form

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import { Navbar,  Nav, Container } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-       
         <Navbar bg="dark" variant="dark" fixed="top" >
           <Container>
             <Navbar.Brand href="#home">Motivational Calendar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="http://localhost:1234/calendar">Calendar</Nav.Link>
+              <Nav.Link href="http://localhost:1234/">Calendar</Nav.Link>
               <Nav.Link href="http://localhost:1234/quotes">Quotes</Nav.Link>
               <Nav.Link href="http://localhost:1234/addquote">Add Quote</Nav.Link>
+              <Nav.Link href="http://localhost:1234/login">Log In</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,53 +1,53 @@
 import React, { useState } from 'react';
+import './style.css'
 
 function LoginForm({ Login, error }) {
     const [details, setDetails] = useState({name: "", username: "", password: ""});
-    const submitHandler = async (e) => {
 
+   const submitHandler = async (e) => {
         e.preventDefault();
-
-        console.log(details)
-
+        const data = details
+        console.log('THIS IS THE DATA', data)
         try {
             const response = await fetch('http://localhost:3000/login', {
                 method: 'POST',
-                headers: {'Content-Type': 'applications/json'},
-                body: JSON.stringify(details)
-            }).then((res) => {
-                console.log(res)
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data)
             })
-            console.log(response)
         } catch(err) {
             console.error(err)
             alert('Invalid login')
         }
-
-
     }
+
+
     return (
-        <form onSubmit={submitHandler}>
-            <div ClassName="form-inner">
+        <form class="sticky-top" onSubmit={submitHandler}>
+            <div className="form-inner">
                 <h2>Login</h2>
                 {(error !== "") ? ( <div className="error">{error}</div> ) : ""}
                 <div className="form-group">
-                    <label htmlFor="name">Name:</label>
+                    <label>Name:</label>
                     <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
                 </div>
                    <div className="form-group">
-                       <label htmlFor="username">Username: </label>
+                       <label>Username: </label>
                        <input type="text" name="username" id="username" onChange={e => setDetails({...details, username: e.target.value})} value={details.username} />
                        </div>
                        <div className="form-group">
                            <label htmlFor="password">Password: </label>
                            <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
                         </div>
-                        <p class="form__text">
-                            <a href="#" class="form__link">Forgot your Password?</a>
+                        <p className="form__text">
+                            <a href="#" className="form__link">Forgot your Password?</a>
                         </p>
-                        <p class="form__text">
-                            <a class="form__link" id="linkCreateAccount" >Don't have an account? Create account</a>
+                        <p className="form__text">
+                            <a className="form__link" id="linkCreateAccount" >Don't have an account? Create account</a>
                         </p>
-                        <input type="submit" value="LOGIN"/>
+                        <input type="submit"/>
                 </div>
         </form>
     )
